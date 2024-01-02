d91f6690bef978ae7760d47022a9a0336eb16f62
Muhammad Agus Luthfi <muhammadagusluthfi21@gmail.com>
2024-01-02 08:13:55 +0700

    update

---
+++ JavaScript Dasar/js/tipe_data_array.js
@@ -0,0 +1,4 @@
+/* Tipe Data Array 
+Array adalah tipe data yang berisikan kumpulan data
+Array di JavaScipt memilki sifat dinamis, aritnya datanya bisa bertambah
+dengan sendirinya saat kita memasukkan data ke dalam Array */
